13e1155f8b6d36583a0a62aea44991705bfc7a7c
bgranzow <kontakt@bgranzow.de>
2020-03-27 22:36:52 +0100

    Add test value menu as samples

---
+++ Assets/BgTools/Samples/TestValueMenu/Editor/PlayerPreferencesTestMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayerPreferencesTestMenu {
+
+    [MenuItem("Tools/BG Tools/Player Preferences Test Values/AddEntry Strings")]
+    public static void addTestValueString()
+    {
+        PlayerPrefs.SetString("String", "boing");
+        PlayerPrefs.SetString("String2", "foo");
+        PlayerPrefs.Save();
+    }
+
+    [MenuItem("Tools/BG Tools/Player Preferences Test Values/AddEntry Int")]
+    public static void addTestValueInt()
+    {
+        PlayerPrefs.SetInt("Int", 1234);
+        PlayerPrefs.Save();
+    }
+
+    [MenuItem("Tools/BG Tools/Player Preferences Test Values/AddEntry Float")]
+    public static void addTestValueFloat()
+    {
+        PlayerPrefs.SetFloat("float", 3.14f);
+        PlayerPrefs.Save();
+    }
+
+    [MenuItem("Tools/BG Tools/Player Preferences Test Values/DeleteAll")]
+    public static void deleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
