14a24a590ecf80c2a003cbe8236a8f714e78aec8
bgranzow <kontakt@bgranzow.de>
2020-04-16 18:48:18 +0200

    Add sample scene for runtime examples.

---
+++ Packages/PlayerPreferencesEditor/Samples/SampleScene/PlayerPrefsController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+
+    #region Add
+    public void AddTestStrings()
+    {
+        PlayerPrefs.SetString("String", "boing");
+        PlayerPrefs.SetString("String2", "foo");
+        //PlayerPrefs.Save();
+    }
+
+    public void AddTestInt()
+    {
+        PlayerPrefs.SetInt("Int", 1234);
+        //PlayerPrefs.Save();
+    }
+
+    public void AddTestFloat()
+    {
+        PlayerPrefs.SetFloat("Float", 3.14f);
+        //PlayerPrefs.Save();
+    }
+    #endregion
+
+    #region Remove
+    public void RemoveTestStrings()
+    {
+        PlayerPrefs.DeleteKey("String");
+        PlayerPrefs.DeleteKey("String2");
+        //PlayerPrefs.Save();
+    }
+
+    public void RemoveTestInt()
+    {
+        PlayerPrefs.DeleteKey("Int");
+        //PlayerPrefs.Save();
+    }
+
+    public void RemoveTestFloat()
+    {
+        PlayerPrefs.DeleteKey("Float");
+        //PlayerPrefs.Save();
+    }
+
+    public void DeleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+        //PlayerPrefs.Save();
+    }
+    #endregion
+}
